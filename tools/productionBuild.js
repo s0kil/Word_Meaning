2eee205e7cfc6ddb32209d274e3fa524d84732a4
Daniel Sokil <dtechtours@gmail.com>
2020-07-05 17:42:37 -0400

    Mint Install Deps On Prod Build

--- tools/productionBuild.js
+++ tools/productionBuild.js
@@ -5,4 +5,5 @@ shell.exec(
   "wget --no-verbose -O mint https://mint-lang.s3-eu-west-1.amazonaws.com/mint-latest-linux"
 );
 shell.chmod("+x", "mint");
+shell.exec("./mint install");
 shell.exec("./mint build --skip-service-worker");
